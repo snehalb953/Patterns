bf3444308506c9fce8d2ef73ec8c26f770149797
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:37:58 +0530

    Add files via upload

---
+++ PatternString.java
@@ -0,0 +1,55 @@
+/*import java.util.Scanner;
+class PatternString
+{
+  public static void main(String[] args)
+   {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter number");
+     int n=sc.nextInt();
+     for(int i=1;i<=n;i++)
+       {
+         for(int j=1;j<=n;j++)
+             {
+               System.out.print((char)(i+96)+" ");
+              }
+          System.out.println();
+       }
+  }
+}
+*/
+/* OUTPUT
+a a a a a
+b b b b b
+c c c c c
+d d d d d
+e e e e e
+*/
+
+
+
+/*import java.util.Scanner;
+class PatternString
+{
+  public static void main(String[] args)
+   {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter number");
+     int n=sc.nextInt();
+     for(int i=1;i<=n;i++)
+       {
+         for(int j=1;j<=n;j++)
+             {
+               System.out.print((char)(j+64)+" ");
+              }
+          System.out.println();
+       }
+  }
+}
+*/
+/*OUTPUT
+A B C D E
+A B C D E 
+A B C D E
+A B C D E
+A B C D E
+*/
