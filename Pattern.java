f7f9fc8b113b19bc717240be7107f11ab593abbe
snehalb953 <snehalb953@gmail.com>
2026-07-05 12:37:18 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,179 @@
+/*
+import java.util.Scanner;
+class Pattern
+{
+  public static void main(String[] args)
+   {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter number");
+     int n=sc.nextInt();
+     for(int i=1;i<=n;i++)
+       {
+         for(int j=1;j<=n;j++)
+             {
+               System.out.print(i%2+" ");
+              }
+          System.out.println();
+       }
+  }
+}
+*/
+/* OUTPUT
+1 1 1 1 1
+0 0 0 0 0
+1 1 1 1 1 
+0 0 0 0 0
+1 1 1 1 1
+*/
+
+
+
+/*
+import java.util.Scanner;
+class Pattern
+{
+  public static void main(String[] args)
+   {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter number");
+     int n=sc.nextInt();
+     for(int i=1;i<=n;i++)
+       {
+         for(int j=1;j<=n;j++)
+             {
+               System.out.print(j%2+" ");
+              }
+          System.out.println();
+       }
+  }
+}
+*/
+/*
+OUTPUT
+1 0 1 0 1
+1 0 1 0 1
+1 0 1 0 1
+1 0 1 0 1
+1 0 1 0 1
+*/
+
+
+
+/*
+import java.util.Scanner;
+class Pattern
+{
+  public static void main(String[] args)
+   {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter number");
+     int n=sc.nextInt();
+     for(int i=n;i>=1;i--)
+       {
+         for(int j=n;j>=1;j--)
+             {
+               System.out.print(i+" ");
+              }
+          System.out.println();
+       }
+  }
+}
+*/
+/*
+OUTPUT 
+5 5 5 5 5
+4 4 4 4 4
+3 3 3 3 3
+2 2 2 2 2
+1 1 1 1 1
+*/
+
+
+
+
+/*
+import java.util.Scanner;
+class Pattern
+{
+  public static void main(String[] args)
+   {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter number");
+     int n=sc.nextInt();
+     for(int i=n;i>=1;i--)
+       {
+         for(int j=n;j>=1;j--)
+             {
+               System.out.print(j+" ");
+              }
+          System.out.println();
+       }
+  }
+}
+*/
+/*OUTPUT
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+*/
+
+
+
+/*
+import java.util.Scanner;
+class Pattern
+{
+  public static void main(String[] args)
+   {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter number");
+     int n=sc.nextInt();
+     for(int i=5;i>=1;i--)
+       {
+          for(int j=1;j<=5;j++)
+             {
+                System.out.print(i+","+j+" ");
+             }
+           System.out.println();
+       }
+   }
+}
+*/
+/*OUTPUT
+5,1 5,2 5,3 5,4 5,5
+4,1 4,2 4,3 4,4 4,5
+3,1 3,2 3,3 3,4 3,5
+2,1 2,2 2,3 2,4 2,5
+1,1 1,2 1,3 1,4 1,5
+*/
+
+
+/*
+import java.util.Scanner;
+class Pattern
+{
+  public static void main(String[] args)
+   {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter number");
+     int n=sc.nextInt();
+     for(int i=1;i<=n;i++)
+       {
+          for(int j=1;j<=3;j++)
+             {
+                System.out.print(i+","+j+" ");
+             }
+           System.out.println();
+       }
+   }
+}
+*/
+/* OUTPUT
+1,1 1,2 1,3
+2,1 2,2 2,3
+3,1 3,2 3,3
+4,1 4,2 4,3
+5,1 5,2 5,3
+*/
